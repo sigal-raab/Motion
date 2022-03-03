Temporary hack in AnimationStructure.joint_children(), bugfix in Quaternions.interpolate()

AnimationStructure.py
@@ -121,7 +121,7 @@ def children_list(parents):
     """
     
     def joint_children(i):
-        return [j for j, p in enumerate(parents) if p == i]
+        return [j for j, p in enumerate(parents) if not isinstance(p, tuple) and p == i]  # todo: 'isinstance' is a hack. change later
         
     return list(map(lambda j: np.array(joint_children(j)), joints(parents)))
      

Quaternions.py
@@ -211,7 +211,7 @@ class Quaternions:
         return self
     
     def interpolate(self, ws):
-        return Quaternions.exp(np.average(abs(self).log, axis=0, weights=ws))
+        return Quaternions.exp(np.average(abs(self).log(), axis=0, weights=ws)[np.newaxis])
     
     def euler(self, order='xyz'):
         
@@ -336,7 +336,7 @@ class Quaternions:
     @classmethod
     def exp(cls, ws):
     
-        ts = np.sum(ws**2.0, axis=-1)**0.5
+        ts = np.sum(ws**2.0, axis=-1)**0.5 # compute the norm for each entry. equivalent to np.linalg.norm(ws, axis=-1)
         ts[ts == 0] = 0.001
         ls = np.sin(ts) / ts
         

test.py
@@ -267,7 +267,7 @@ def test_anim_from_pose():
 
 def test_Quat():
     from Quaternions import Quaternions
-    order = 'yzx'
+    order = 'xyz'
     for t in np.arange(100):
 
         # degrees_from = np.random.random(3)*360-180 # sample degrees in [-180,180]
@@ -301,15 +301,16 @@ def test_Quat():
         assert np.isclose(q_from.qs[0,0], r.as_quat()[-1])
         assert np.allclose(q_from.qs[0,1:], r.as_quat()[:3])
         assert np.allclose(degrees2_to, degrees_to_scipy)
+        print(degrees_from)
     pass
 
 
 if __name__ == '__main__':
-    # test_Quat()
+    test_Quat()
     # IK_test_minimal()
     # IK_test_consistency_scipy()
     # IK_test_consistency_Quaternions()
-    IK_test_consistency() # clean
+    # IK_test_consistency() # clean
     # test_anim_from_pose()
     # pose_2d = np.array([[473.68356, 444.9424],
     #                       [500.9961, 448.02988],